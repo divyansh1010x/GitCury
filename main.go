516307cf16a3e8ac6479acb77f1852427fb8678b
Lakshya Jain <lakshyajainrj19@gmail.com>
2025-04-04 18:52:01 +0530

    Feat: Add new endpoints for commit and push

--- main.go
+++ main.go
@@ -18,9 +18,12 @@ func main() {
 
 	router.HandleFunc("/config", handlers.ConfigHandler).Methods("GET")
 	router.HandleFunc("/config", handlers.ConfigHandler).Methods("POST")
-	router.HandleFunc("/getmessages", handlers.PrepareCommitMessagesHandler).Methods("GET")
-	router.HandleFunc("/commit", handlers.CommitAllFiles).Methods("GET")
-	router.HandleFunc("/commit", handlers.CommitFolder).Methods("POST")
+	router.HandleFunc("/getallmsgs", handlers.PrepareCommitMessagesHandler).Methods("GET")
+	router.HandleFunc("/getonemsgs", handlers.PrepareCommitMessagesOne).Methods("Get")
+	router.HandleFunc("/commitall", handlers.CommitAllFiles).Methods("GET")
+	router.HandleFunc("/commitone", handlers.CommitFolder).Methods("Get")
+	router.HandleFunc("/pushall", handlers.PushAll).Methods("POST")
+	router.HandleFunc("/pushfolder", handlers.PushOne).Methods("POST")
 
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
