4cad4e311a8b8b42e66cc934f9139d014852b261
Lakshya Jain <lakshyajainrj19@gmail.com>
2025-04-08 16:02:53 +0530

    refactor: Improve logging and error messages

--- core/msgs.go
+++ core/msgs.go
@@ -16,11 +16,11 @@ func GetAllMsgs(numFiles ...int) error {
 		numFiles[0] = defaultNumFiles
 	}
 
-	utils.Debug("Number of files to prepare commit messages for: " + strconv.Itoa(numFiles[0]))
+	utils.Debug("[GENESIS]: Preparing commit messages for " + strconv.Itoa(numFiles[0]) + " files per folder.")
 
 	rootFolders, ok := config.Get("root_folders").([]interface{})
 	if !ok {
-		utils.Error("Invalid or missing root_folders configuration")
+		utils.Error("[GENESIS]: Invalid or missing root_folders configuration.")
 		return fmt.Errorf("invalid or missing root_folders configuration")
 	}
 
@@ -31,7 +31,7 @@ func GetAllMsgs(numFiles ...int) error {
 	for _, rootFolder := range rootFolders {
 		rootFolderStr, ok := rootFolder.(string)
 		if !ok {
-			utils.Error("Invalid root folder type")
+			utils.Error("[GENESIS]: Invalid root folder type.")
 			continue
 		}
 
@@ -39,11 +39,11 @@ func GetAllMsgs(numFiles ...int) error {
 		go func(folder string) {
 			defer rootFolderWg.Done()
 
-			utils.Debug("Root folder to get messages : " + folder)
+			utils.Debug("[GENESIS]: Processing root folder: " + folder)
 
 			changedFiles, err := git.GetAllChangedFiles(folder)
 			if err != nil {
-				utils.Error("Failed to get changed files: " + err.Error())
+				utils.Error("[GENESIS]: Failed to retrieve changed files for folder '" + folder + "' - " + err.Error())
 				mu.Lock()
 				errors = append(errors, fmt.Sprintf("Folder: %s, Error: %s", folder, err.Error()))
 				mu.Unlock()
@@ -51,7 +51,7 @@ func GetAllMsgs(numFiles ...int) error {
 			}
 
 			if len(changedFiles) == 0 {
-				utils.Info("No changed files found")
+				utils.Debug("[GENESIS]: No changed files found in folder: " + folder)
 				return
 			}
 
@@ -59,11 +59,11 @@ func GetAllMsgs(numFiles ...int) error {
 				changedFiles = changedFiles[:numFiles[0]]
 			}
 
-			utils.Debug("Total files to process: " + strconv.Itoa(len(changedFiles)))
+			utils.Debug("[GENESIS]: Total files to process in folder '" + folder + "': " + strconv.Itoa(len(changedFiles)))
 
 			err = git.BatchProcessGetMessages(changedFiles, folder)
 			if err != nil {
-				utils.Error("Batch processing failed for folder: " + folder + ", Error: " + err.Error())
+				utils.Error("[GENESIS]: Batch processing failed for folder '" + folder + "' - " + err.Error())
 				mu.Lock()
 				errors = append(errors, fmt.Sprintf("Folder: %s, Error: %s", folder, err.Error()))
 				mu.Unlock()
@@ -74,71 +74,69 @@ func GetAllMsgs(numFiles ...int) error {
 	rootFolderWg.Wait()
 
 	if len(errors) > 0 {
-		utils.Error("Batch processing completed with errors")
-		utils.Debug("Errors: " + fmt.Sprint(errors))
+		utils.Error("[GENESIS]: Batch processing completed with errors.")
+		utils.Debug("[GENESIS]: Errors encountered: " + fmt.Sprint(errors))
 		return fmt.Errorf("one or more errors occurred while preparing commit messages")
 	}
 
-	utils.Info("Batch generation of all messages completed successfully")
+	utils.Success("[GENESIS]: Commit message generation completed successfully for all folders.")
 	output.SaveToFile()
 	return nil
 }
 
 func GetMsgsForRootFolder(folder string, numFiles ...int) error {
 	if folder == "" {
-		utils.Error("Root folder is empty")
+		utils.Error("[GENESIS]: Root folder is empty.")
 		return fmt.Errorf("root folder is empty")
 	}
 
 	numFilesToCommit := 10 // Default value
 	if len(numFiles) > 0 && numFiles[0] > 0 {
-		utils.Debug("Using provided number of files to commit: " + strconv.Itoa(numFiles[0]))
+		utils.Debug("[GENESIS]: Using provided number of files to commit: " + strconv.Itoa(numFiles[0]))
 		numFilesToCommit = numFiles[0]
 	} else {
 		if configValue := config.Get("numFilesToCommit"); configValue != "" {
 			if configValueFloat, ok := configValue.(float64); ok {
-				utils.Debug("Using config value for numFilesToCommit: " + strconv.FormatFloat(configValueFloat, 'f', -1, 64))
+				utils.Debug("[GENESIS]: Using config value for numFilesToCommit: " + strconv.FormatFloat(configValueFloat, 'f', -1, 64))
 				numFilesToCommit = int(configValueFloat)
 			} else if configValueStr, ok := configValue.(string); ok {
 				if parsedValue, err := strconv.Atoi(configValueStr); err == nil {
-					utils.Debug("Using config value for numFilesToCommit from string: " + configValueStr)
+					utils.Debug("[GENESIS]: Using config value for numFilesToCommit from string: " + configValueStr)
 					numFilesToCommit = parsedValue
 				} else {
-					utils.Error("Invalid string value for numFilesToCommit: " + configValueStr)
+					utils.Error("[GENESIS]: Invalid string value for numFilesToCommit: " + configValueStr)
 				}
 			}
 		}
 	}
 
-	utils.Debug("Number of files to prepare commit messages for: " + strconv.Itoa(numFilesToCommit))
-
-	utils.Debug("Root folder to get messages : " + folder)
+	utils.Debug("[GENESIS]: Preparing commit messages for " + strconv.Itoa(numFilesToCommit) + " files in folder: " + folder)
 
 	changedFiles, err := git.GetAllChangedFiles(folder)
 	if err != nil {
-		utils.Error("Failed to get changed files: " + err.Error())
+		utils.Error("[GENESIS]: Failed to retrieve changed files for folder '" + folder + "' - " + err.Error())
 		return fmt.Errorf("failed to get changed files: %s", err.Error())
 	}
 
 	if len(changedFiles) == 0 {
-		utils.Info("No changed files found")
+		utils.Debug("[GENESIS]: No changed files found in folder: " + folder)
 		return nil
 	}
 
-	if len(changedFiles) > numFiles[0] {
+	if len(changedFiles) > numFilesToCommit {
 		changedFiles = changedFiles[:numFilesToCommit]
 	}
 
-	utils.Debug("Total files to process: " + strconv.Itoa(len(changedFiles)))
+	utils.Debug("[GENESIS]: Total files to process in folder '" + folder + "': " + strconv.Itoa(len(changedFiles)))
 
 	err = git.BatchProcessGetMessages(changedFiles, folder)
 	if err != nil {
-		utils.Error("Batch processing failed: " + err.Error())
+		utils.Error("[GENESIS]: Batch processing failed for folder '" + folder + "' - " + err.Error())
 		return fmt.Errorf("batch processing failed: %s", err.Error())
 	}
 
-	utils.Info(fmt.Sprintf("Batch generation of all messages for %s completed successfully", folder))
-	utils.Debug(fmt.Sprintf("All output : %v", output.GetAll()))
+	utils.Success("[GENESIS]: Commit message generation completed successfully for folder: " + folder)
+	utils.Debug("[GENESIS]: All output: " + fmt.Sprint(output.GetAll()))
 	output.SaveToFile()
 	return nil
 }
