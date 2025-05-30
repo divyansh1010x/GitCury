6f6c85a47880bb78da3654ad1ab26b7d395706f4
Lakshya Jain <lakshyajainrj19@gmail.com>
2025-05-30 09:51:03 +0530

    feat: Add GeminiRunner dependency injection

---
+++ di/gemini.go
@@ -0,0 +1,16 @@
+package di
+
+import "GitCury/interfaces"
+
+// GeminiRunnerInstance allows dependency injection for testing
+var GeminiRunnerInstance interfaces.GeminiRunner
+
+// SetGeminiRunner allows injecting a custom GeminiRunner (used in tests)
+func SetGeminiRunner(runner interfaces.GeminiRunner) {
+	GeminiRunnerInstance = runner
+}
+
+// GetGeminiRunner returns the current Gemini runner instance
+func GetGeminiRunner() interfaces.GeminiRunner {
+	return GeminiRunnerInstance
+}
